6b0d630ea9273d638f02cfc64ba3fe9cb857e32d
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-08 19:24:50 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,18 @@
 def unsafe?(speed)
+  
+  if speed < 40 || speed > 60
+    return TRUE
+  else
+    return FALSE
+  end
 
 end
 
 
 
 def not_safe?(speed)
+  
+  speed < 40 || speed > 60 ? TRUE : FALSE 
 	
 end
 	
